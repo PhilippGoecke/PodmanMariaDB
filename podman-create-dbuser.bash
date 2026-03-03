4cc3e9b7336ebe742f1ba79cde000adada01289a
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-03-03 21:15:06 +0100

    Parameterize user and database creation script

--- podman-create-dbuser.bash
+++ podman-create-dbuser.bash
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+USERNAME="your_user"
+DBNAME="your_database"
+ROOT_PASSWORD="your_password"
+USER_PASSWORD="your_user_password"
+
 # Check if user exists
-if ! podman exec mariadb mysql -uroot -pyour_password -e "SELECT User FROM mysql.user WHERE User='your_user'" | grep -q your_user; then
+if ! podman exec mariadb mysql -uroot -p"$ROOT_PASSWORD" -e "SELECT User FROM mysql.user WHERE User='$USERNAME'" | grep -q "$USERNAME"; then
     echo "Creating user..."
-    podman exec mariadb mysql -uroot -pyour_password -e "CREATE USER 'your_user'@'%' IDENTIFIED BY 'your_user_password';"
-    podman exec mariadb mysql -uroot -pyour_password -e "GRANT ALL PRIVILEGES ON your_database.* TO 'your_user'@'%'; FLUSH PRIVILEGES;"
+    podman exec mariadb mysql -uroot -p"$ROOT_PASSWORD" -e "CREATE USER '$USERNAME'@'%' IDENTIFIED BY '$USER_PASSWORD';"
+    podman exec mariadb mysql -uroot -p"$ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $DBNAME.* TO '$USERNAME'@'%'; FLUSH PRIVILEGES;"
 else
     echo "User already exists"
 fi
 
 # Check if database exists
-if ! podman exec mariadb mysql -uroot -pyour_password -e "USE your_database" &>/dev/null; then
+if ! podman exec mariadb mysql -uroot -p"$ROOT_PASSWORD" -e "USE $DBNAME" &>/dev/null; then
     echo "Creating database..."
-    podman exec mariadb mysql -uroot -pyour_password -e "CREATE DATABASE your_database;"
+    podman exec mariadb mysql -uroot -p"$ROOT_PASSWORD" -e "CREATE DATABASE $DBNAME;"
 else
     echo "Database already exists"
 fi
