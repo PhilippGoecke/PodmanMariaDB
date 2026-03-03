d4ab66adc1e208cfea143162ed267ee41a3f191c
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-03-03 20:58:12 +0100

    Add check for existing MariaDB container
    Added a check to see if the MariaDB container is already running before starting it.

--- podman-run-mariadb.bash
+++ podman-run-mariadb.bash
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Starte MariaDB Container
-podman run --detach \
-    --name mariadb \
-    --env MARIADB_ROOT_PASSWORD=your_password \
-    --env MARIADB_USER=example-user \
-    --env MARIADB_PASSWORD=my_cool_secret \
-    --env MARIADB_DATABASE=exmple-database \
-    --publish 3306:3306 \
-    -v mariadb_data:/var/lib/mysql \
-    docker.io/library/mariadb:latest
+# Check if MariaDB container is already running
+if ! podman ps | grep -q mariadb; then
+    # Starte MariaDB Container
+    podman run --detach \
+        --name mariadb \
+        --env MARIADB_ROOT_PASSWORD=your_password \
+        --env MARIADB_USER=example-user \
+        --env MARIADB_PASSWORD=my_cool_secret \
+        --env MARIADB_DATABASE=exmple-database \
+        --publish 3306:3306 \
+        -v mariadb_data:/var/lib/mysql \
+        docker.io/library/mariadb:latest
+fi
