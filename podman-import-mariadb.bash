dd2c66ca421562271b71716a23fab3ca3b03ebda
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-03-04 14:04:54 +0100

    Update podman-import-mariadb.bash

--- podman-import-mariadb.bash
+++ podman-import-mariadb.bash
@@ -5,6 +5,7 @@ DB_PORT=3306
 DB_USERNAME=your_username
 DB_PASSWORD=your_password
 DBNAME="your_database"
-BACKUP_FILE="your_backup_file.sql"
-
-podman run -it --rm docker.io/library/mariadb:latest mariadb -h $DB_HOST -P $DB_PORT -u$DB_USERNAME -p"$DB_PASSWORD" $DBNAME < "$BACKUP_FILE"
+# BACKUP_FILE="your_backup_file.sql"
+# podman run -it --rm docker.io/library/mariadb:latest mariadb -h $DB_HOST -P $DB_PORT -u$DB_USERNAME -p"$DB_PASSWORD" $DBNAME < "$BACKUP_FILE"
+BACKUP_FILE="your_backup_file.sql.gz"
+gunzip -c "$BACKUP_FILE" | podman run -it --rm docker.io/library/mariadb:latest mariadb -h $DB_HOST -P $DB_PORT -u$DB_USERNAME -p"$DB_PASSWORD" $DBNAME
